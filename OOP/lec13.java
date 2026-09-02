d966ab71211a592da0020d41d497924f660b6193
rohan shinde <pradipshinde75494@gmail.com>
2026-09-02 22:48:46 +0530

    New added

--- OOP/lec13.java
+++ OOP/lec13.java
@@ -1,8 +1,8 @@
-package OOP;
+//package OOP;
 
 public class lec13 {
     public static void main(String[] args) {
-        Student s1=new Student();
+        Student s1=new Student("mohan",20,101,"peda");
         System.out.println(s1.name);
         System.out.println(s1.age);
         System.out.println(s1.rollno);
@@ -14,4 +14,12 @@ class Student{
     int age;
     int rollno;
     String college;
+    Student( String a,int n,int b,String c)
+    {
+        name=a;
+        age=n;
+        rollno=b;
+        college=c;
+
+    }
 }
