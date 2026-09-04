fd03c0ceef0f7afc7ad7dea214019f8699850157
rohan shinde <pradipshinde75494@gmail.com>
2026-09-04 21:27:18 +0530

    lec 15

---
+++ OOP/lec16.java
@@ -0,0 +1,15 @@
+public class lec16 {
+    public static void main(String[] args) {
+       BankAccount ba=new BankAccount();
+       ba.balance=10000.00; 
+       System.out.println(ba.balance);
+    }
+    
+}
+class BankAccount{
+ double balance;
+//     void deposit(int amount)
+//     {
+//         balance t=amount;
+//     }
+}
