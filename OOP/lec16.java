0d006a1a7b637fd014d292b40dbb11cd8a6aee63
rohan shinde <pradipshinde75494@gmail.com>
2026-09-04 21:32:32 +0530

    lec 15 get balance term

--- OOP/lec16.java
+++ OOP/lec16.java
@@ -1,15 +1,24 @@
 public class lec16 {
     public static void main(String[] args) {
        BankAccount ba=new BankAccount();
-       ba.balance=10000.00; 
-       System.out.println(ba.balance);
+       ba.deposit(500);
+       ba.withdraw(300);
+       System.out.println(ba.getbalance());
     }
     
 }
 class BankAccount{
- double balance;
-//     void deposit(int amount)
-//     {
-//         balance t=amount;
-//     }
+  private double balance;
+  public  void deposit(int amount)
+    {
+        balance +=amount;
+    }
+    public void withdraw(int amount)
+    {
+        balance-=amount;
+    }
+    public double getbalance()
+    {
+        return balance;
+    }
 }
