be68cc4d9a8b370e8bbc820913fa3890c7efa615
rohan shinde <pradipshinde75494@gmail.com>
2026-09-11 23:34:59 +0530

    last occurance index of targeted element

---
+++ DSAwithGFG/BS/lastOCC.java
@@ -0,0 +1,20 @@
+
+
+public class lastOCC {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,5,5,6,7,7,8};
+        int l=0;
+        int t=5;
+        int r=arr.length;
+        int mid=0;
+   
+      for(int  j=r-1;j>=0;j--)
+      {
+        if(arr[j]==t)
+        {
+            System.out.println(j);
+            break;
+        }
+      }
+    }
+}
