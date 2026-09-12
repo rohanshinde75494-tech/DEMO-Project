fe7d700eddd1154932358d0f61b46348e2bdc7ed
rohan shinde <pradipshinde75494@gmail.com>
2026-09-12 22:55:42 +0530

    pettern printing

--- EMERGENCY/tewlwve.java
+++ EMERGENCY/tewlwve.java
@@ -3,5 +3,15 @@
 public class tewlwve {
     public static void main(String[] args) {
         System.out.println("hello world");
+    
+    int n=5;
+    for(int i=0;i<5;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
     }
+}
 }
