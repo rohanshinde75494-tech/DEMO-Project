037514e2a208e9fecffac80bb6682cd1f94ee598
rohan shinde <pradipshinde75494@gmail.com>
2026-09-12 23:07:20 +0530

    pattern printing ulta triangle

--- EMERGENCY/tewlwve.java
+++ EMERGENCY/tewlwve.java
@@ -34,5 +34,19 @@ public class tewlwve {
 
         System.out.println();
     }
+    System.out.println();
+     for(int i=0;i<5;i++)
+    {
+        for(int k=0;k<i;k++)
+        {
+            System.out.print(" ");
+        }
+        for(int j=n;j>i;j--)
+        {
+            System.out.print("* ");
+        }
+
+        System.out.println();
+    }
 }
 }
