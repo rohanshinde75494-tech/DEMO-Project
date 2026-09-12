79974f91b5aea765275ae58f9fdf70ae2ba6b34f
rohan shinde <pradipshinde75494@gmail.com>
2026-09-12 22:58:45 +0530

    pattern printing triangle

--- EMERGENCY/tewlwve.java
+++ EMERGENCY/tewlwve.java
@@ -13,5 +13,14 @@ public class tewlwve {
         }
         System.out.println();
     }
+     for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<i+1;j++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+
 }
 }
