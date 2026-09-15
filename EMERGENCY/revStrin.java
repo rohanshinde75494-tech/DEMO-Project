027bb3305cecfb478c960030464041cd94280281
rohan shinde <pradipshinde75494@gmail.com>
2026-09-15 23:47:26 +0530

    reverse a string

---
+++ EMERGENCY/revStrin.java
@@ -0,0 +1,12 @@
+public class revStrin {
+    public static void main(String[] args) {
+        String str = "Java";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        System.out.println(rev);
+    }
+}
