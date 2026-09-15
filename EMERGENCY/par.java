5ca1573f3aec63eebd4f9fd77d946c28719c45c5
rohan shinde <pradipshinde75494@gmail.com>
2026-09-15 23:39:48 +0530

    array printed

---
+++ EMERGENCY/par.java
@@ -0,0 +1,11 @@
+public class par {
+    public static void main(String[] args) {
+        System.out.println("Ptint array");
+        int arr[]={1,2,3,4,45};
+        for(int i=0;i<arr.length;i++)
+
+            {
+                System.out.println(arr[i]);
+            }
+    }
+}
