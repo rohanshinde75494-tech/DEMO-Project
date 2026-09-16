6d038a02c2c9f50348a1bd12968c108a6b64a1bb
rohan shinde <pradipshinde75494@gmail.com>
2026-09-16 23:24:03 +0530

    count digit

---
+++ EMERGENCY/count.java
@@ -0,0 +1,13 @@
+public class count {
+    public static void main(String[] args) {
+        int n = 123456;
+int count = 0;
+
+while (n > 0) {
+    count++;
+    n /= 10;
+}
+
+System.out.println(count);
+    }
+}
