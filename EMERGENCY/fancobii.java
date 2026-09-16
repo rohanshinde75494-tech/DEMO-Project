3adff4d2b14a70ad075cc0e681e48af026c3ca91
rohan shinde <pradipshinde75494@gmail.com>
2026-09-16 23:27:43 +0530

    finding fancobii series

---
+++ EMERGENCY/fancobii.java
@@ -0,0 +1,14 @@
+public class fancobii {
+    public static void main(String[] args) {
+        int a = 0;
+int b = 1;
+
+for (int i = 0; i < 10; i++) {
+    System.out.print(a + " ");
+    
+    int c = a + b;
+    a = b;
+    b = c;
+}
+    }
+}
