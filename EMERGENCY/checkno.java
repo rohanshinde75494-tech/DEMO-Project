1b2d4074e61833a4230caef6eb246e1a77568ae4
rohan shinde <pradipshinde75494@gmail.com>
2026-09-16 23:33:37 +0530

    check no is +ve ot -ve

---
+++ EMERGENCY/checkno.java
@@ -0,0 +1,12 @@
+public class checkno {
+    public static void main(String[] args) {
+        int n = -10;
+
+if (n > 0)
+    System.out.println("Positive");
+else if (n < 0)
+    System.out.println("Negative");
+else
+    System.out.println("Zero");
+    }
+}
