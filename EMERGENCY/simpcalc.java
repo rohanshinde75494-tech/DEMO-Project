7b4c482e303576799d167f622910a162c6ee661c
rohan shinde <pradipshinde75494@gmail.com>
2026-09-17 22:56:08 +0530

    simple calculator using switch case

---
+++ EMERGENCY/simpcalc.java
@@ -0,0 +1,29 @@
+public class simpcalc {
+     public static void main(String[] args) {
+
+        int a = 20;
+        int b = 5;
+        char op = '*';
+
+        switch (op) {
+            case '+':
+                System.out.println(a + b);
+                break;
+
+            case '-':
+                System.out.println(a - b);
+                break;
+
+            case '*':
+                System.out.println(a * b);
+                break;
+
+            case '/':
+                System.out.println(a / b);
+                break;
+
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+}
