24286b3ab4eecad129be30d104ecf9dd3f704d2f
rohan shinde <pradipshinde75494@gmail.com>
2026-09-17 22:58:39 +0530

    sum of array elements

---
+++ EMERGENCY/sumarr.java
@@ -0,0 +1,13 @@
+public class sumarr {
+     public static void main(String[] args) {
+
+        int arr[] = {10, 20, 30, 40};
+        int sum = 0;
+
+        for (int x : arr) {
+            sum += x;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
