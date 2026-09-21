06c37079fc0fb799d42161bf6a1c31a8af5b2427
rohan shinde <pradipshinde75494@gmail.com>
2026-09-21 23:42:09 +0530

    print array element

--- EMERGENCY/addnum.java
+++ EMERGENCY/addnum.java
@@ -6,5 +6,10 @@ public class addnum {
         System.out.println("Sum = " + (a + b));
 
         System.out.println("rohan");
+
+        int[] arr = {3,5,6,7,34,21,45,3,22};
+        for(int ele : arr){
+            System.out.println(ele);
+        }
     }
 }
