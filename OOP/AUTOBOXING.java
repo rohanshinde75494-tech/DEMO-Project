70fbda2ee43f73530d2654f04ab697c640e5223e
rohan shinde <pradipshinde75494@gmail.com>
2026-09-23 22:59:55 +0530

    checking .equals keyword

--- OOP/AUTOBOXING.java
+++ OOP/AUTOBOXING.java
@@ -1,16 +1,22 @@
 public class AUTOBOXING {
     public static void main(String[] args) {
-        int x=10;  //AUTOBOXING
-        Integer y=x;
-        System.out.println(x);
-        System.out.println(y); //unboxing
+        // int x=10;  //AUTOBOXING
+        // Integer y=x;
+        // System.out.println(x);
+        // System.out.println(y); //unboxing
 
-        Integer a=20;     //autoboxing
-        int b=a;                  //unboxing
-        System.out.println(a);
-        System.out.println(b);
+        // Integer a=20;     //autoboxing
+        // int b=a;                  //unboxing
+        // System.out.println(a);
+        // System.out.println(b);
 
-         printInteger(b);
+        //  printInteger(b);
+
+        Integer x=10;
+        Integer y=10;
+         System.out.println(x==y);
+        System.out.println(x.equals(y));
+       
        
     }
      static void printInteger(Integer x)
