2736779ed6f7e55498a70b6141a32fd7fe5237b8
rohan shinde <pradipshinde75494@gmail.com>
2026-09-23 22:28:53 +0530

    autoboxing concept

---
+++ OOP/AUTOBOXING.java
@@ -0,0 +1,8 @@
+public class AUTOBOXING {
+    public static void main(String[] args) {
+        int x=10;  //AUTOBOXING
+        Integer y=x;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
