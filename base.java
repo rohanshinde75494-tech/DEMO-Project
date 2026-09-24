275c4548f95356a5eab476e4886871a8b218cf66
rohan shinde <pradipshinde75494@gmail.com>
2026-09-24 23:37:54 +0530

    add string

--- base.java
+++ base.java
@@ -2,5 +2,7 @@ public class base {
      public static void main(String[] args) {
         System.out.println("Demo project");
         System.out.println("Hi Rohan");
+        String str = "aman";
+        
      }
 }
